1cfe0631f6997655998a8be3d84316ccbaf67c7b
August Miller <hello@gusmiller.com>
2024-09-20 10:50:27 -0700

    Crude coordinate readout in Leaflet This is inaccurate at non-default zoom levels!

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,15 +16,67 @@ L.tileLayer('/api/tiles/{z}/{x}/{y}', {
 // Add a home marker:
 L.marker([0, 0]).addTo(map);
 
-map.on('mousemove', function(e) {
-    const dimensions = map.getSize();
-    const blockScale = 16;
+// Set up our coordinate output:
+L.Control.WorldCoordinates = L.Control.extend({
+    map: null,
+    $block: null,
+    $chunk: null,
+    $region: null,
 
-    Math.pow(16, 1 / (4 - map.getZoom()));
+    _bindings: {},
 
-    console.log({
-        x: e.layerPoint.x - (dimensions.x / 2),
-        y: e.layerPoint.y - (dimensions.y / 2),
-        zoom: map.getZoom(),
-    });
+    onAdd: function(map) {
+        this.map = map;
+        const $container = L.DomUtil.create('div');
+
+        this.$block = L.DomUtil.create('div');
+        this.$chunk = L.DomUtil.create('div');
+        this.$region = L.DomUtil.create('div');
+
+        $container.appendChild(this.$block);
+        $container.appendChild(this.$chunk);
+        $container.appendChild(this.$region);
+
+        $container.style = `padding: 10px; background-color: white; min-width: 15em; border-radius: 2px;`;
+        $container.style.visibility = 'hidden';
+
+        map.on('mousemove', this._bindings.onMouseMove = this.onMapMouseMove.bind(this));
+        map.on('mouseover', this._bindings.onMapMouseOver = this.onMapMouseOver.bind(this));
+        map.on('mouseout', this._bindings.onMapMouseOut = this.onMapMouseOut.bind(this));
+
+        return $container;
+    },
+
+    onRemove: function(map) {
+        map.off('mousemove', this._bindings.onMapMouseMove);
+        map.off('mouseover', this._bindings.onMapMouseOver);
+        map.off('mouseout', this._bindings.onMapMouseOut);
+
+        this.map = null;
+    },
+
+    onMapMouseOver: function(e) {
+        this.getContainer().style.visibility = 'visible';
+    },
+
+    onMapMouseMove: function(e) {
+        const dimensions = map.getSize();
+        const blockScale = Math.pow(2, this.map.getZoom() - 1);
+
+        const normalizedMouseX = e.layerPoint.x - (dimensions.x / 2);
+        const normalizedMouseY = e.layerPoint.y - (dimensions.y / 2);
+
+        const worldX = normalizedMouseX / blockScale;
+        const worldZ = normalizedMouseY / blockScale;
+
+        this.$block.innerText = `Block: ${Math.floor(worldX)}, ${Math.floor(worldZ)}`;
+        this.$chunk.innerText = `Chunk: ${Math.floor(worldX / 16)}, ${Math.floor(worldZ / 16)}`;
+        this.$region.innerText = `Region: ${Math.floor(worldX / 16 / 32)}, ${Math.floor(worldZ / 16 / 32)}`;
+    },
+
+    onMapMouseOut: function(e) {
+        this.getContainer().style.visibility = 'hidden';
+    },
 });
+
+(new L.Control.WorldCoordinates({ position: 'bottomleft' })).addTo(map);
