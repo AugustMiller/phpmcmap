6649eb227f238dca2d8cc3f7ea47bcf034e8f71f
August Miller <hello@gusmiller.com>
2024-09-28 15:40:53 -0700

    Fix Vite HMR config?

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-const port = 5172;
-const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
+const port = 5173;
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
         }),
     ],
     server: {
-        // respond to all network requests
         host: true,
         port: port,
         strictPort: true,
-        // Defines the origin of the generated asset URLs during development,
-        // this will also be used for the public/hot file (Vite devserver URL)
-        // origin,
         hmr: {
-            // host: process.env.DDEV_PRIMARY_URL,
-            // host: `${process.env.DDEV_HOSTNAME}:${port}`,
             host: process.env.DDEV_HOSTNAME,
-            // server: 'wss',
+            protocol: 'wss',
         },
     },
 });
