6cb02549576f7793b1c9fa479ac9ce4710cb76a2
August Miller <hello@gusmiller.com>
2025-08-21 09:16:01 -0700

    Dedicated RCON command

---
+++ app/Console/Commands/Rcon.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use xPaw\SourceQuery\SourceQuery;
+
+class Rcon extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:rcon {str}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a command to the RCON server';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $rcon = new SourceQuery;
+
+        try {
+            $rcon->Connect(env('MC_RCON_SERVER'), env('MC_RCON_PORT'));
+            $rcon->SetRconPassword(env('MC_RCON_PASSWORD'));
+
+            $rcon->Rcon($this->argument('str'));
+        } catch(\Exception $e) {
+            $this->error("Failed to connect to the RCON server: {$e->getMessage()}");
+        } finally {
+            $rcon->Disconnect();
+        }
+    }
+}
