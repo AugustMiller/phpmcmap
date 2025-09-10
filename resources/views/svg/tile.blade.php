6b3fbf55a36d50a111dcb21a11445de04107ad11
August Miller <hello@gusmiller.com>
2025-09-10 06:33:10 -0700

    Default color inline

--- resources/views/svg/tile.blade.php
+++ resources/views/svg/tile.blade.php
@@ -2,18 +2,13 @@
 <svg width="256" height="256" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <title>{{ $title }}</title>
 
-    <style>
-        .ground {
-            fill: rgb(50, 50, 50);
-        }
-    </style>
-
     <rect
         x="0"
         y="0"
         width="256"
         height="256"
-        class="ground"></rect>
+        class="ground"
+        fill="rgb(50, 50, 50)"></rect>
 
     @foreach ($rects as $rect)
         <rect
