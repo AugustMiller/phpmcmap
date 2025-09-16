f486cef06bb127fed8df61c697935d2eeb93b93a
August Miller <hello@gusmiller.com>
2025-09-15 22:23:36 -0700

    Fix sign rendering

--- resources/js/entities/sign.js
+++ resources/js/entities/sign.js
@@ -2,7 +2,6 @@ import { createSymbolIcon } from './util';
 
 export default function createSignMarker(entity) {
     const lines = entity.metadata.front_text.messages
-        .map(m => JSON.parse(m))
         .filter(l => l.length);
 
     const marker = L.marker([-entity.z, entity.x], {
