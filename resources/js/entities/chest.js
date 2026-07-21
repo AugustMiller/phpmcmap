8276af6408d87a3d75ef5e593ef6d6bcc068c342
August Miller <hello@gusmiller.com>
2026-07-21 14:16:37 -0700

    Storage container labels

--- resources/js/entities/chest.js
+++ resources/js/entities/chest.js
@@ -1,9 +1,11 @@
-import { createSymbolIcon } from './util';
+import { cleanEntityId, createSymbolIcon } from './util';
 
 export default function createChestMarker(entity) {
     if (typeof entity.metadata.Items === 'undefined') {
         throw new Error('Not enough metadata present to render.');
     }
+
+    const type = cleanEntityId(entity.entity_type);
 
     const total = entity.metadata.Items.reduce(function(total, slot) {
         const count = slot.count || slot.Count;
@@ -12,11 +14,13 @@ export default function createChestMarker(entity) {
     }, 0);
 
     const marker = L.marker([-entity.z, entity.x], {
-        title: `Chest`,
-        icon: createSymbolIcon('chest'),
+        title: `Container (${type})`,
+        icon: createSymbolIcon('container'),
     });
 
-    marker.bindPopup(`Chest: ${total} item(s)`);
+    const message = total === 0 ? `Empty container (${type})` : `Container (${type}): ${total} item(s)`;
+
+    marker.bindPopup(message);
 
     return marker;
 };
